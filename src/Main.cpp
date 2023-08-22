2b51458d91a6a53872e2099dbd1f6fa9899579bc
aiden <aidenrphotos@gmail.com>
2023-08-21 20:25:58 -0400

    Add main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,41 +1,9 @@
-#include "Platform/Platform.hpp"
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	util::Platform platform;
-
-#if defined(_DEBUG)
-	std::cout << "Hello World!" << std::endl;
-#endif
-
-	sf::RenderWindow window;
-	// in Windows at least, this must be called before creating the window
-	float screenScalingFactor = platform.getScreenScalingFactor(window.getSystemHandle());
-	// Use the screenScalingFactor
-	window.create(sf::VideoMode(200.0f * screenScalingFactor, 200.0f * screenScalingFactor), "SFML works!");
-	platform.setIcon(window.getSystemHandle());
-
-	sf::CircleShape shape(window.getSize().x / 2);
-	shape.setFillColor(sf::Color::White);
-
-	sf::Texture shapeTexture;
-	shapeTexture.loadFromFile("content/sfml.png");
-	shape.setTexture(&shapeTexture);
-
-	sf::Event event;
-
-	while (window.isOpen())
-	{
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
-
-		window.clear();
-		window.draw(shape);
-		window.display();
-	}
-
-	return 0;
+	float screen_width = 960;
+	float screen_height = 540;
+	sf::RenderWindow window(sf::VideoMode(screen_width, screen_height), "Dawn of Matti");
+	window.setFramerateLimit(60);
 }
